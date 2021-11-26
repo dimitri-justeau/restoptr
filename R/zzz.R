6c8b74fa2c790f05a14615a5a8c46d4e28244745
Dimitri <dimitri.justeau@gmail.com>
2021-11-26 17:11:56 +1100

    Initial commit

---
+++ R/zzz.R
@@ -0,0 +1,17 @@
+.onLoad <- function(libname, pkgname) {
+  # Check that Java version is at least 8
+  .jinit()
+  jv <- .jcall("java/lang/System", "S", "getProperty", "java.runtime.version")
+  if(substr(jv, 1L, 2L) == "1.") {
+    jvn <- as.numeric(paste0(strsplit(jv, "[.]")[[1L]][1:2], collapse = "."))
+    if(jvn < 1.8) stop("Java >= 8 is needed for this package but not available")
+  }
+  # Download restopt jar if not already downloaded
+  jar_path <- file.path(libname, pkgname, "java", "restopt-1.0-SNAPSHOT.jar")
+  if (!file.exists(jar_path)) {
+    old_options <- options(timeout = max(1000, getOption("timeout")))
+    on.exit(options(old_options))
+    utils::download.file("https://github.com/dimitri-justeau/restopt/releases/download/1.0-beta/restopt-1.0-SNAPSHOT.jar", destfile = jar_path)
+  }
+  .jpackage(pkgname, lib.loc = libname)
+}
