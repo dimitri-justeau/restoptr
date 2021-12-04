13c784e05fac1a3e90975b7420f6cdd0908162c4
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2021-12-04 21:58:10 +1300

    draft code for solve() function - also remove compile function - this is because the restopt api will generating a solution once we specify the obj, so can't build and solve the problem in seperate steps

--- R/solve.R
+++ R/solve.R
@@ -17,15 +17,61 @@ solve.RestoptProblem <- function(a, b, ...) {
   # assert argument is valid
   assertthat::assert_that(inherits(a, "RestoptProblem"))
 
-  # compile problem
-  o <- restopt_compile(a)
+  # assert argument is valid
+  assertthat::assert_that(inherits(x, "RestoptProblem"))
 
-  # solve problem
-  # TODO
+  # prepare data
+  ## check if data already saved to disk
+  eh_on_disk <- terra_on_disk(x$data$existing_habitat)
+  rh_on_disk <- terra_on_disk(x$data$restorable_habitat)
+  ## save rasters to disk if needed
+  eh_data <- terra_force_disk(x$data$existing_habitat)
+  rh_data <- terra_force_disk(x$data$existing_habitat)
 
-  # prepare result
-  # TODO
+  # import data
+  jdata <- rJava::.jnew(
+    "org.restopt.DataLoader",
+    problem@habitat_path,
+    problem@accessible_path,
+    problem@restorable_path
+  )
+  # initialize problem
+  jproblem <-rJava::.jnew(
+    "org.restopt.BaseProblem", jdata, problem@accessible_value
+  )
 
-  # return result
-  # TODO
+  # add constraints
+  for (i in seq_along(x$constraints)) {
+    x$constraints[[i]]$post(jproblem)
+  }
+
+  # add objective and solve the problem
+  output_path <- tempfile()
+  result <- try(
+    x$objective$post(
+      jproblem, x$settings$precision, x$settings$time_limit, output_path
+    ),
+    silent = TRUE
+  )
+
+  # throw error if failed
+  if (inherits(result, "try-error")) {
+    stop("failed to complete optimization")
+  }
+
+  # import results
+  r <- terra::rast(paste0(output_path, ".tif"))
+  attributes(r)$metadata <- utils::read.csv(paste0(output, ".csv"))
+
+  # clean up
+  if (eh_on_disk) {
+    rm(eh_data)
+    unlink(terra::sources(eh_data)$source[[1]])
+  }
+  if (rh_on_disk) {
+    rm(rh_data)
+    unlink(terra::sources(rh_data)$source[[1]])
+  }
+
+
 }
