7704bde43300efaf2ec8775d9a7d1a453a78af04
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2021-12-12 18:06:51 +1300

    update onLoad fn - put restopt jar download URL in variable to simplify updates for future versions - print package start up message if downloading file to explain why a file is being downloaded - save jar file in directory  that (i) is CRAN approved and (ii) users have write permissions (users might not be able to write into pkg dir if pkg installed by HPC admin)

--- R/zzz.R
+++ R/zzz.R
@@ -3,20 +3,51 @@
   if (is.null(getOption("java.parameters"))) {
     options(java.parameters = "-Xmx4g")
   }
-  # Check that Java version is at least 8
-  .jinit()
-  jv <- .jcall("java/lang/System", "S", "getProperty", "java.runtime.version")
-  if(substr(jv, 1L, 2L) == "1.") {
+
+  # initialize java
+  rJava::.jinit()
+
+  # check Java version is acceptable
+  jv <- rJava::.jcall(
+    "java/lang/System", "S", "getProperty", "java.runtime.version"
+  )
+  if (substr(jv, 1L, 2L) == "1.") {
     jvn <- as.numeric(paste0(strsplit(jv, "[.]")[[1L]][1:2], collapse = "."))
-    if(jvn < 1.8) stop("Java >= 8 is needed for this package but not available")
+    if (jvn < 1.8) {
+      stop("Java (version 8+) is not available for the restoptr package")
+    }
   }
-  # Download restopt jar if not already downloaded
-  jar_path <- file.path(libname, pkgname, "java", "restopt-1.0-SNAPSHOT.jar")
+
+  # specify file path for restopt jar file
+  jar_path <- file.path(
+    tools::R_user_dir("restoptr", which = "cache"),
+    basename(restopt_url)
+  )
+
+  # download restopt jar file if needed
+  # see https://github.com/dimitri-justeau/restopt
   if (!file.exists(jar_path)) {
+    ## create directory to store jar file if needed
+    if (!file.exists(dirname(jar_path))) {
+      dir.create(dirname(jar_path), showWarnings = FALSE, recursive = TRUE)
+    }
+    ## print message to explain why files are being downloaded
+    packageStartupMessage(
+      "Downloading Java software to generate prioritizations..."
+    )
+    ## download jar file
     old_options <- options(timeout = max(1000, getOption("timeout")))
     on.exit(options(old_options))
-    utils::download.file("https://github.com/dimitri-justeau/restopt/releases/download/1.0-beta/restopt-1.0-SNAPSHOT.jar", destfile = jar_path)
+    utils::download.file(restopt_url, destfile = jar_path, mode = "wb")
   }
-  .jpackage(pkgname, lib.loc = libname)
-  J("java.lang.System")$setProperty("EPSG-HSQL.directory", tempdir())
+
+  # initialize Java for package
+  rJava::.jpackage(pkgname, lib.loc = libname)
+  rJava::J("java.lang.System")$setProperty("EPSG-HSQL.directory", tempdir())
 }
+
+# define donwload URL for restopt jae file
+restopt_url <- paste0(
+  "https://github.com/dimitri-justeau/restopt/releases/download/1.0-beta/",
+  "restopt-1.0-SNAPSHOT.jar"
+)
