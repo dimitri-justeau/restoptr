f9a923d01a297ed0616d806f5e616df47abe5e7e
Dimitri <dimitri.justeau@gmail.com>
2022-03-08 18:27:55 +1100

    Documentation for add_settings https://github.com/dimitri-justeau/restoptr/issues/20

--- R/add_settings.R
+++ R/add_settings.R
@@ -3,7 +3,7 @@ NULL
 
 #' Add settings
 #'
-#' Add settings to a restoration problem ([restopt_problem()] object
+#' Add settings to a restoration problem ([restopt_problem()]) object
 #' to customize the optimization procedure.
 #'
 #' @inheritParams add_max_mesh_objective
@@ -16,8 +16,28 @@ NULL
 #' Defaults to 0.
 #'
 #' @examples
-#' \dontrun{TODO}
+#' \dontrun{
+#' # load data
+#' habitat_data <- rast(
+#'   system.file("extdata", "habitat.tif", package = "restoptr")
+#' )
 #'
+#' restorable_data <- rast(
+#'   system.file("extdata", "restorable.tif", package = "restoptr")
+#' )
+#'
+#' # plot data
+#' plot(rast(list(habitat_data, restorable_data)), nc = 2)
+#'
+#' # create problem
+#' p <- restopt_problem(
+#'        existing_habitat = habitat_data,
+#'        restorable_habitat = restorable_data
+#' ) %>%
+#' add_settings(time_limit = 1, precision = 4)
+#' # print problem
+#' print(p)
+#' }
 #' @export
 add_settings <- function(problem, precision = 4, time_limit = 0) {
   # assert argument is valid
