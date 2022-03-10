79116c761771681f37504fd95ede4a3c5bff33d6
Dimitri <dimitri.justeau@gmail.com>
2022-03-10 16:49:00 +1100

    Only run tests using landscapemetrics if the package is available

--- tests/testthat/test_add_components_constraint.R
+++ tests/testthat/test_add_components_constraint.R
@@ -7,25 +7,33 @@ test_that("add_components_constraint", {
   problem <- restopt_problem(habitat, restorable) %>%
     add_components_constraint(min_nb_components = 1, max_nb_components = 1)
   result <- solve(problem)
-  np <- landscapemetrics::lsm_c_np(result, directions = 4)
-  testthat::expect_equal(np[np$class == 2,]$value, 1)
+  if (require(landscapemetrics)) {
+    np <- landscapemetrics::lsm_c_np(result, directions = 4)
+    testthat::expect_equal(np[np$class == 2,]$value, 1)
+  }
   # Create problem with 2 components
   problem <- restopt_problem(habitat, restorable) %>%
     add_components_constraint(min_nb_components = 2, max_nb_components = 2)
   result <- solve(problem)
-  np <- landscapemetrics::lsm_c_np(result, directions = 4)
-  testthat::expect_equal(np[np$class == 2,]$value, 2)
+  if (require(landscapemetrics)) {
+    np <- landscapemetrics::lsm_c_np(result, directions = 4)
+    testthat::expect_equal(np[np$class == 2,]$value, 2)
+  }
   # Create problem with 5 components
   problem <- restopt_problem(habitat, restorable) %>%
     add_components_constraint(min_nb_components = 5, max_nb_components = 5)
   result <- solve(problem)
-  np <- landscapemetrics::lsm_c_np(result, directions = 4)
-  testthat::expect_equal(np[np$class == 2,]$value, 5)
+  if (require(landscapemetrics)) {
+    np <- landscapemetrics::lsm_c_np(result, directions = 4)
+    testthat::expect_equal(np[np$class == 2,]$value, 5)
+  }
   # Create problem with between 6 and 10 components
   problem <- restopt_problem(habitat, restorable) %>%
     add_components_constraint(min_nb_components = 6, max_nb_components = 10)
   result <- solve(problem)
-  np <- landscapemetrics::lsm_c_np(result, directions = 4)
-  testthat::expect_gte(np[np$class == 2,]$value, 6)
-  testthat::expect_lte(np[np$class == 2,]$value, 10)
+  if (require(landscapemetrics)) {
+    np <- landscapemetrics::lsm_c_np(result, directions = 4)
+    testthat::expect_gte(np[np$class == 2,]$value, 6)
+    testthat::expect_lte(np[np$class == 2,]$value, 10)
+  }
 })
--- tests/testthat/test_add_connected_constraint.R
+++ tests/testthat/test_add_connected_constraint.R
@@ -1,14 +1,16 @@
 context("restopt_problem")
 
-test_that("add_components_constraint", {
+test_that("add_connected_constraint", {
   # Create problem without optimization
   habitat <- terra::rast(system.file("extdata", "habitat.tif", package = "restoptr"))
   restorable <- terra::rast(system.file("extdata", "restorable.tif", package = "restoptr"))
   problem <- restopt_problem(habitat, restorable) %>%
     add_connected_constraint()
   result <- solve(problem)
-  np <- landscapemetrics::lsm_c_np(result, directions = 4)
-  testthat::expect_equal(np[np$class == 2,]$value, 1)
+  if (require(landscapemetrics)) {
+    np <- landscapemetrics::lsm_c_np(result, directions = 4)
+    testthat::expect_equal(np[np$class == 2,]$value, 1)
+  }
   # Create problem with optimization
   habitat <- terra::rast(system.file("extdata", "habitat.tif", package = "restoptr"))
   restorable <- terra::rast(system.file("extdata", "restorable.tif", package = "restoptr"))
@@ -16,6 +18,8 @@ test_that("add_components_constraint", {
     add_connected_constraint() %>%
     add_max_mesh_objective()
   result <- solve(problem)
-  np <- landscapemetrics::lsm_c_np(result, directions = 4)
-  testthat::expect_equal(np[np$class == 2,]$value, 1)
+  if (require(landscapemetrics)) {
+    np <- landscapemetrics::lsm_c_np(result, directions = 4)
+    testthat::expect_equal(np[np$class == 2,]$value, 1)
+  }
 })
