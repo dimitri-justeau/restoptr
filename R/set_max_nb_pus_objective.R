30244bc931c70d4780ac48e51c8b8974501acd1e
Dimitri <dimitri.justeau@gmail.com>
2022-05-18 16:13:23 +0200

    Implement and test set_max_nb_pus_objective

---
+++ R/set_max_nb_pus_objective.R
@@ -0,0 +1,78 @@
+#' @include internal.R
+NULL
+
+#' Set an objective to maximize the number of planning units
+#'
+#' Specify that a restoration problem ([restopt_problem()]) should
+#' maximize the number of planning units.
+#'
+#' @param problem [restopt_problem()] Restoration problem object.
+#'
+#' @details Planning units correspond to aggregated cells from the original
+#' dataset. Maximizing the number of planning units increased the spatial extent
+#' of the restoration area. This can be useful when the budget is limited and
+#' the aim is to restore the larget possible extent.
+#'
+#' @return An updated restoration problem ([restopt_problem()]) object.
+#'
+#' @examples
+#' \dontrun{
+#' # load data
+#' habitat_data <- rast(
+#'   system.file("extdata", "habitat_hi_res.tif", package = "restoptr")
+#' )
+#'
+#' locked_out_data <- rast(
+#'  system.file("extdata", "locked_out.tif", package = "restoptr")
+#' )
+#'
+#' # plot data
+#' plot(rast(list(habitat_data, restorable_data, locked_out_data)), nc = 3)
+#'
+#' # create problem with locked out constraints
+#' p <- restopt_problem(
+#'     existing_habitat = habitat_data,
+#'     aggregation_factor = 16,
+#'     habitat_threshold = 0.7
+#'   ) %>%
+#'   set_max_nb_pus_objective() %>%
+#'   add_restorable_constraint(
+#'     min_restore = 5,
+#'     max_restore = 5,
+#'   ) %>%
+#'   add_locked_out_constraint(data = locked_out_data) %>%
+#'   add_settings(time_limit = 1)
+#'
+#' # print problem
+#' print(p)
+#'
+#' # solve problem
+#' s <- solve(p)
+#'
+#' # plot solution
+#' plot(
+#'   x = s, main = "solution",
+#'   col = c("#E5E5E5", "#fff1d6", "#b2df8a", "#1f78b4")
+#' )
+#' }
+#'
+#' @export
+set_max_nb_pus_objective <- function(problem) {
+  # assert argument is valid
+  assertthat::assert_that(inherits(problem, "RestoptProblem"))
+
+  # set objective
+  set_restopt_objective(
+    problem = problem,
+    objective = restopt_component(
+      name = "Maximize number of planning units",
+      class = c("MaxNbPusObjective", "RestoptObjectve"),
+      post = function(jproblem, nb_solutions, precision, time_limit, optimality_gap,
+                      verbose=FALSE) {
+        rJava::.jcall(
+          jproblem, "Ljava/util/List;", "maximizeNbPUS", nb_solutions, time_limit, optimality_gap, verbose
+        )
+      }
+    )
+  )
+}
---
+++ tests/testthat/test_set_max_nb_pus_objective.R
@@ -0,0 +1,35 @@
+context("minimize_nb_pus")
+
+test_that("expected_result", {
+  # import data
+  habitat_data <- terra::rast(
+    system.file("extdata", "habitat_hi_res.tif", package = "restoptr"
+  ))
+  accessible_data <- terra::vect(system.file(
+    "extdata", "accessible_areas.gpkg", package = "restoptr"
+  ))
+  # prepare locked out data
+  locked_out_data <- invert_vector(
+    accessible_data,
+    extent = terra::ext(habitat_data),
+    filter = accessible_data$ID == 1
+  )
+  # build and solve problem
+  problem <-
+    restopt_problem(habitat_data, 0.7, 16) %>%
+    add_locked_out_constraint(locked_out_data) %>%
+    add_components_constraint(min_nb_components = 1, max_nb_components = 1) %>%
+    add_compactness_constraint(max_diameter = 2700, unit = "m") %>%
+    add_restorable_constraint(
+      min_restore = 90, max_restore = 110, unit = "ha", min_proportion = 0.7
+    ) %>%
+    set_max_nb_pus_objective() %>%
+    add_settings(time_limit = 10)
+  result <- solve(problem)
+  md <- get_metadata(result, area_unit = "ha")
+  # tests
+  expect_is(result, "RestoptSolution")
+  expect_lte(md$solving_time, 10 * 1.1)
+  expect_gte(md$min_restore, set_units(90, "ha"))
+  expect_lte(md$min_restore, set_units(110, "ha"))
+})
