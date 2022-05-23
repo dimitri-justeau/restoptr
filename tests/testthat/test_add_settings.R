5c295d623fa95b74a3f5fe4acc1a3a863090a960
Jeff Hanson <jeffrey.hanson@uqconnect.edu.au>
2022-05-24 07:23:34 +1200

    add vignette (#51) (fix #36)
    * add vign
    * rebuild docs
    * fix cmd for local install, bug with submodules preventing local install, see https://github.com/r-lib/remotes/issues/236
    * more tests for add_settings
    * remove uneeded space in DESCRIPTION

--- tests/testthat/test_add_settings.R
+++ tests/testthat/test_add_settings.R
@@ -71,3 +71,55 @@ test_that("nb_solutions", {
   expect_gte(terra::global(result[[1]] == 3, "sum", na.rm = TRUE), 1)
   expect_gte(terra::global(result[[2]] == 3, "sum", na.rm = TRUE), 1)
 })
+
+test_that("optimality_gap", {
+  # import data
+  habitat_data <- terra::rast(
+    system.file("extdata", "habitat_hi_res.tif", package = "restoptr"
+  ))
+  accessible_data <- terra::vect(system.file(
+    "extdata", "accessible_areas.gpkg", package = "restoptr"
+  ))
+  locked_out_data <- invert_vector(accessible_data, extent = ext(habitat_data))
+  # build and solve problem
+  problem <-
+    restopt_problem(habitat_data, 0.7, 16) %>%
+    add_compactness_constraint(4.4, unit = "km") %>%
+    add_locked_out_constraint(locked_out_data) %>%
+    add_components_constraint(min_nb_components = 1, max_nb_components = 1) %>%
+    set_no_objective() %>%
+    add_settings(time_limit = 5, optimality_gap = 0.5)
+  result <- solve(problem, verbose = TRUE)
+  # tests
+  expect_is(result, "RestoptSolution")
+  expect_gte(terra::global(result == 3, "sum", na.rm = TRUE), 1)
+})
+
+test_that("settings successfully overwritten", {
+  # import data
+  habitat_data <- terra::rast(
+    system.file("extdata", "habitat_hi_res.tif", package = "restoptr"
+  ))
+  # build problems
+  p1 <-
+    restopt_problem(habitat_data, 0.7, 16) %>%
+    add_settings(
+      time_limit = 5, nb_solutions = 3, optimality_gap = 0.5, precision = 3
+    )
+  p2 <-
+    p1 %>%
+    add_settings(
+      time_limit = 4, nb_solutions = 5, optimality_gap = 0.1, precision = 2
+    )
+  # tests
+  ## p1
+  expect_equal(p1$settings$time_limit, 5)
+  expect_equal(p1$settings$nb_solutions, 3)
+  expect_equal(p1$settings$optimality_gap, 0.5)
+  expect_equal(p1$settings$precision, 3)
+  ## p2
+  expect_equal(p2$settings$time_limit, 4)
+  expect_equal(p2$settings$nb_solutions, 5)
+  expect_equal(p2$settings$optimality_gap, 0.1)
+  expect_equal(p2$settings$precision, 2)
+})
